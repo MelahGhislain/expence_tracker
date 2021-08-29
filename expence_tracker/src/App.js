daebfceba698454724a84cb9ae5b5f373186e59d
MelahGhislain <melahghislain17@gmail.com>
2021-08-29 15:26:57 +0100

    still working on the filter

--- expence_tracker/src/App.js
+++ expence_tracker/src/App.js
@@ -2,41 +2,63 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import Board from "./components/Board";
 
-const expenses = [
-  {
-    id: 1,
-    date: new Date(2020, 3, 20),
-    title: "Rent",
-    amount: 200.78,
-  },
-  {
-    id: 2,
-    date: new Date(2019, 2, 30),
-    title: "Insurance",
-    amount: 20.22,
-  },
-  {
-    id: 3,
-    date: new Date(2021, 5, 15),
-    title: "Phone",
-    amount: 10.08,
-  },
-];
+// const expenses = [
+//   {
+//     id: 1,
+//     date: new Date(2020, 3, 20),
+//     title: "Rent",
+//     amount: 200.78,
+//   },
+//   {
+//     id: 2,
+//     date: new Date(2019, 2, 30),
+//     title: "Insurance",
+//     amount: 20.22,
+//   },
+//   {
+//     id: 3,
+//     date: new Date(2021, 5, 15),
+//     title: "Phone",
+//     amount: 10.08,
+//   },
+// ];
 
 export default class App extends Component {
-
-  getExpenseData = (expenseItem) =>{
-    console.log("From App")
-    console.log(expenseItem)
-    return expenseItem
+  constructor(props) {
+    super(props);
+    this.state = {
+      filteredExpenses: [],
+      expenses: [
+        {
+          id: 1,
+          date: new Date(2020, 3, 20),
+          title: "Rent",
+          amount: 200.78,
+        },
+      ],
+    };
   }
+  getFilteredData = (expenseItems) => {
+    this.setState({ filteredExpenses: [...expenseItems] });
+  };
+
+  getExpenseData = (expenseItem) => {
+    this.setState({ expenses: [expenseItem, ...this.state.expenses] });
+  };
 
   render() {
     return (
       <div className="app">
-        <div className="container"> 
-          <Header getData={this.getExpenseData}/>
-          <Board expenses={expenses}  />
+        <div className="container">
+          <Header getData={this.getExpenseData} />
+          <Board
+            expenses={
+              this.state.filteredExpenses === []
+                ? this.state.filteredExpenses
+                : this.state.expenses
+            }
+            getFilteredData={this.getFilteredData}
+          />
         </div>
       </div>
     );
